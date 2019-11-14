ae1e58ec0c946bf59994ae7768005b2a70d49be9
Krunal Patel <Pkrunal616@gmail.com>
2019-11-14 17:48:41 -0500

    deleting movie work

---
+++ client/src/Movies/UpdateMovie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UpdateMovie = props => {
+
+    return(
+        <div>
+            <h1>Update The Movie</h1>
+        </div>
+    )
+}
+
+export default UpdateMovie;
